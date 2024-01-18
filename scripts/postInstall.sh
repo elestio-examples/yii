47af420ab4c00a8e2417a696c0c32bc437cf61cd
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-01-18 17:54:30 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -9,24 +9,23 @@ sleep 60s;
 
 docker-compose down
 
-# cat << EOT > ./basic/config/db.php 
+cat << EOT > ./basic/config/db.php 
+<?php
 
-# <?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=yii',
+    'username' => 'yii',
+    'password' => '${DATABASE_PASSWORD}',
+    'charset' => 'utf8',
 
-# return [
-#     'class' => 'yii\db\Connection',
-#     'dsn' => 'mysql:host=mysql;dbname=yii',
-#     'username' => 'root',
-#     'password' => '${DATABASE_PASSWORD}',
-#     'charset' => 'utf8',
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
 
-#     // Schema cache options (for production environment)
-#     //'enableSchemaCache' => true,
-#     //'schemaCacheDuration' => 60,
-#     //'schemaCache' => 'cache',
-# ];
-
-# EOT
+EOT
 
 docker-compose run --rm php composer update --prefer-dist
 docker-compose run --rm php composer install    
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,9 +1,4 @@
 # set env vars
 set -o allexport; source .env; set +o allexport;
 
-git clone https://github.com/yiisoft/yii2-app-basic.git basic
-
-rm ./basic/docker-compose.yml
-rm ./basic/.github
-
 chmod -R 777 ./basic/
