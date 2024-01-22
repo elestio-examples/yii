48b7b89d6093852c5dbfd4247f8b5e5df29d8c6a
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-01-22 11:37:05 +0200

    fix

--- scripts/postDeploy.sh
+++ scripts/postDeploy.sh
@@ -1,11 +1,6 @@
-rm -rf ./basic/config
-cp -R ./temp/config ./basic/config
-
-rm -rf ./temp
-
 docker-compose down;
 
-docker-compose run --rm php composer update --prefer-dist
+docker-compose run --rm php composer update
 docker-compose run --rm php composer install    
 
 docker-compose up -d
--- scripts/preDeploy.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p ./temp
-cp -R ./basic/config ./temp/config
